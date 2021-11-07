2a6d0415141fea71b59140f96f14cae157757348
Hirsch Ábel <h045844@stud.u-szeged.hu>
2021-11-07 19:21:39 +0100

    Termékek kereséséhez/szűréséhez tartozó üzleti logika

--- dao/guest-dao.js
+++ dao/guest-dao.js
@@ -6,9 +6,19 @@ class GuestDAO {
 		return result.rows;
 	}
 
+	async getTags() {
+		let result = await db.query(`SELECT * FROM tagek`).catch(console.log);
+		return result.rows;
+	}
+
 	async getOneProduct(nev) {
 		let result = await db.query(`SELECT * FROM termek WHERE nev = $1`,[nev]).catch(console.log);
 		return result.rows[0];
+	}
+
+	async getNevProduct(nev) {
+		let result = await db.query(`SELECT * FROM termek WHERE nev = $1`,[nev]).catch(console.log);
+		return result.rows;
 	}
 
 	async updateProduct(nev, ar, leiras, kep) {
@@ -19,6 +29,11 @@ class GuestDAO {
 	async deleteDog(nev) {
 		await db.query(`DELETE FROM termek WHERE nev=$1`,[nev]).catch(console.log);
         return; 
+	}
+
+	async getTagsByID(tag_nev) {
+		let result = await db.query(`SELECT termek.nev, termek.ar, termek.leiras, termek.kep FROM tagek, tag_kapcs, termek WHERE tagek.id = tag_kapcs.id AND tag_kapcs.nev = termek.nev AND tagek.nev = '${tag_nev}';`).catch(console.log);
+		return result.rows;
 	}
 };
 
